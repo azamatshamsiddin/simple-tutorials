2aa62a60cbe6a6dc90e38d5138ce74404f2dbb8e
Azamat <muratovichazamat@yandex.com>
2022-03-06 15:48:46 +0500

    initial commit

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,25 @@
+const btns = document.querySelectorAll(".btn");
+const value = document.getElementById("value");
+
+let count = 0;
+btns.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count < 0) {
+      value.style.color = "red";
+    } else {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
